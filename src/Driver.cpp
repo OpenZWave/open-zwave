e12a79216bce8005937a2d6a2e7cf9cbc61b4198
google@lansell.org <google@lansell.org>
2010-02-18 20:09:00 +0000

    OpenZWave henceforth licensed under the GNU Lesser General Public License.

--- src/Driver.cpp
+++ src/Driver.cpp
@@ -11,16 +11,16 @@
 //	This file is part of OpenZWave.
 //
 //	OpenZWave is free software: you can redistribute it and/or modify
-//	it under the terms of the GNU General Public License as published
+//	it under the terms of the GNU Lesser General Public License as published
 //	by the Free Software Foundation, either version 3 of the License,
 //	or (at your option) any later version.
 //
 //	OpenZWave is distributed in the hope that it will be useful,
 //	but WITHOUT ANY WARRANTY; without even the implied warranty of
 //	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//	GNU General Public License for more details.
+//	GNU Lesser General Public License for more details.
 //
-//	You should have received a copy of the GNU General Public License
+//	You should have received a copy of the GNU Lesser General Public License
 //	along with OpenZWave.  If not, see <http://www.gnu.org/licenses/>.
 //
 //-----------------------------------------------------------------------------
@@ -1186,13 +1186,13 @@ void Driver::HandleApplicationUpdateRequest
 )
 {
 	uint8 nodeId = _data[3];
-	SetNodeAwake( nodeId );
 
 	switch( _data[2] )
 	{
 		case UPDATE_STATE_NODE_INFO_RECEIVED:
 		{
 			Log::Write( "UPDATE_STATE_NODE_INFO_RECEIVED from node %d", nodeId );
+			SetNodeAwake( nodeId );
 			if( Node* node = m_nodes[nodeId] )
 			{
 				node->UpdateNodeInfo( &_data[8], _data[4] - 3 );
@@ -1210,6 +1210,7 @@ void Driver::HandleApplicationUpdateRequest
 			Log::Write( "** Network change **: ID %d was assigned to a new Z-Wave node", nodeId );
 			delete m_nodes[nodeId];
 			m_nodes[nodeId] = new Node( nodeId );
+			SetNodeAwake( nodeId );
 			break;
 		}
 		case UPDATE_STATE_DELETE_DONE:
