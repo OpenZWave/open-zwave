0df87c55c8a4eb0c7a7f5f405dd39a4a8cfe1e12
djbrown2001 <djbrown2001@db0adee0-878e-11de-bed6-6d316fbe0868>
2010-04-14 03:33:59 +0000

    Committed OpenZWaveDLL, CSharp Wrappers, and demo code.

---
+++ wrappers/dot_net/pinvoke/OZWrapperLib/ZWaveValueId.cs
@@ -0,0 +1,147 @@
+﻿#region Header
+//-----------------------------------------------------------------------------
+//
+//      ZWaveValueID.cs
+//
+//      Thin wrapper for the C++ OpenZWave ValueID class
+//
+//      Copyright (c) 2010 Doug Brown <djbrown2001@gmail.com>
+//
+//      SOFTWARE NOTICE AND LICENSE
+//
+//      This file is part of OpenZWave.
+//
+//      OpenZWave is free software: you can redistribute it and/or modify
+//      it under the terms of the GNU Lesser General Public License as published
+//      by the Free Software Foundation, either version 3 of the License,
+//      or (at your option) any later version.
+//
+//      OpenZWave is distributed in the hope that it will be useful,
+//      but WITHOUT ANY WARRANTY; without even the implied warranty of
+//      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//      GNU Lesser General Public License for more details.
+//
+//      You should have received a copy of the GNU Lesser General Public License
+//      along with OpenZWave.  If not, see <http://www.gnu.org/licenses/>.
+//
+//-----------------------------------------------------------------------------
+#endregion Header
+
+using System;
+using System.Runtime.InteropServices;
+
+namespace OpenZWaveWrapper
+{
+	/// <summary>
+	/// Thin wrapper for the C++ OpenZWave ValueID class
+	/// </summary>
+	public class ZWaveValueId
+	{
+		#region PInvokes
+
+		[DllImport("OpenZwaveDLL.dll", SetLastError=true)]
+		private static extern UInt32 OPENZWAVEDLL_GetHomeId(IntPtr ptr);
+
+		[DllImport("OpenZwaveDLL.dll", SetLastError=true)]
+		private static extern byte OPENZWAVEDLL_GetNodeId(IntPtr ptr);
+
+		[DllImport("OpenZwaveDLL.dll", SetLastError=true)]
+		private static extern byte OPENZWAVEDLL_GetCommandClassId(IntPtr ptr);
+
+		[DllImport("OpenZwaveDLL.dll", SetLastError=true)]
+		private static extern byte OPENZWAVEDLL_GetInstance(IntPtr ptr);
+
+		[DllImport("OpenZwaveDLL.dll", SetLastError=true)]
+		private static extern byte OPENZWAVEDLL_GetIndex(IntPtr ptr);
+
+		[DllImport("OpenZwaveDLL.dll", SetLastError=true)]
+		private static extern ValueGenre OPENZWAVEDLL_GetGenre(IntPtr ptr);
+
+		[DllImport("OpenZwaveDLL.dll", SetLastError=true)]
+		private static extern ValueType OPENZWAVEDLL_GetType(IntPtr ptr);
+
+		#endregion	PInvokes
+		
+		#region members
+		private IntPtr m_pValueId;
+		
+		public enum ValueType : byte
+		{
+			ValueType_Bool = 0,
+			ValueType_Byte,
+			ValueType_Decimal,
+			ValueType_Int,
+			ValueType_List,
+			ValueType_Short,
+			ValueType_String,
+			ValueType_Count			
+		};
+		
+		public enum ValueGenre : byte
+		{
+			ValueGenre_All = 0,
+			ValueGenre_User,			// Basic values an ordinary user would be interested in
+			ValueGenre_Config,			// Device-specific configuration parameters
+			ValueGenre_System,			// Values of significance only to users who understand the Z-Wave protocol 
+			ValueGenre_Count
+		};
+		
+		#endregion
+		
+		private ZWaveValueId() {} // not implemented
+			
+		public ZWaveValueId(IntPtr pValueId)
+		{
+			//TODO: throw exception or return return null if pNode is null
+			if (IntPtr.Zero != pValueId)
+			{
+				m_pValueId = pValueId;
+			}
+		}
+		
+		public IntPtr ValueIdRef
+		{
+			get { return m_pValueId; }
+		}
+		
+		#region Wrapper Methods
+		
+		public UInt32 HomeId
+		{
+			get { return OPENZWAVEDLL_GetHomeId(this.m_pValueId); }
+		}
+		
+		public byte NodeId
+		{
+			get { return OPENZWAVEDLL_GetNodeId(this.m_pValueId); }
+		}
+		
+		public ValueGenre Genre
+		{
+			get { return OPENZWAVEDLL_GetGenre(this.m_pValueId); }
+		}
+		
+		public byte CommandClassId
+		{
+			get { return OPENZWAVEDLL_GetCommandClassId(this.m_pValueId); }
+		}
+		
+		public byte Instance
+		{
+			get { return OPENZWAVEDLL_GetInstance(this.m_pValueId); }
+		}
+		
+		public byte Index
+		{
+			get { return OPENZWAVEDLL_GetIndex(this.m_pValueId); }
+		}
+		
+		public ValueType Type
+		{
+			get { return OPENZWAVEDLL_GetType(this.m_pValueId); }
+		}
+		
+		#endregion Wrapper Methods
+		
+	}
+}
---
+++ wrappers/dot_net/pinvoke/OZWrapperLib/ZWaveValueList.cs
@@ -0,0 +1,166 @@
+﻿#region Header
+//-----------------------------------------------------------------------------
+//
+//      ZWaveValueList.cs
+//
+//      Thin wrapper for the C++ OpenZWave ValueList class
+//
+//      Copyright (c) 2010 Doug Brown <djbrown2001@gmail.com>
+//
+//      SOFTWARE NOTICE AND LICENSE
+//
+//      This file is part of OpenZWave.
+//
+//      OpenZWave is free software: you can redistribute it and/or modify
+//      it under the terms of the GNU Lesser General Public License as published
+//      by the Free Software Foundation, either version 3 of the License,
+//      or (at your option) any later version.
+//
+//      OpenZWave is distributed in the hope that it will be useful,
+//      but WITHOUT ANY WARRANTY; without even the implied warranty of
+//      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//      GNU Lesser General Public License for more details.
+//
+//      You should have received a copy of the GNU Lesser General Public License
+//      along with OpenZWave.  If not, see <http://www.gnu.org/licenses/>.
+//
+//-----------------------------------------------------------------------------
+#endregion Header
+
+using System;
+using System.Text;
+using System.Runtime.InteropServices;
+
+namespace OpenZWaveWrapper
+{
+	/// <summary>
+	/// Thin wrapper for the C++ OpenZWave ValueList class
+	/// </summary>
+	public class ZWaveValueList : IDisposable
+	{
+		#region PInvokes
+		[DllImport("OpenZwaveDLL.dll", SetLastError=true)]
+		[return : MarshalAs(UnmanagedType.Bool)] private static extern bool OPENZWAVEDLL_SetListByLabel(IntPtr ptr, string setVal);
+
+		[DllImport("OpenZwaveDLL.dll", SetLastError=true)]
+		[return : MarshalAs(UnmanagedType.Bool)] private static extern bool OPENZWAVEDLL_SetListByValue(IntPtr ptr, Int32 setVal);
+		
+		[DllImport("OpenZwaveDLL.dll", SetLastError=true)]
+		private static extern void OPENZWAVEDLL_OnValueChangedList(IntPtr ptr, Int32 valueIdx);
+		
+		[DllImport("OpenZwaveDLL.dll", SetLastError=true)]
+		private static extern void OPENZWAVEDLL_GetAsStringList(IntPtr ptr, StringBuilder sb);
+		
+		[DllImport("OpenZwaveDLL.dll", SetLastError=true)]
+		private static extern ITEM OPENZWAVEDLL_GetValueListItem(IntPtr ptr);
+		
+		[DllImport("OpenZwaveDLL.dll", SetLastError=true)]
+		private static extern ITEM OPENZWAVEDLL_GetPendingListItem(IntPtr ptr);
+		
+		[DllImport("OpenZwaveDLL.dll", SetLastError=true)]
+		private static extern Int32 OPENZWAVEDLL_GetItemIdxByLabel(IntPtr ptr, string strLabel);
+		
+		[DllImport("OpenZwaveDLL.dll", SetLastError=true)]
+		private static extern Int32 OPENZWAVEDLL_GetItemIdxByValue(IntPtr ptr, Int32 val);
+
+		#endregion	PInvokes
+		
+		#region Members
+		
+		private IntPtr m_pValue;
+		
+		[StructLayout(LayoutKind.Sequential)]
+		public struct ITEM
+		{
+			[MarshalAs(UnmanagedType.ByValTStr, SizeConst = 128)]
+			public string m_label;
+			[MarshalAs(UnmanagedType.I4)]
+			public Int32 m_value;
+		};
+		
+		#endregion Members
+		
+		
+		private ZWaveValueList() {} // Not Implemented
+		
+		public ZWaveValueList( IntPtr pValue)
+		{
+			//TODO: throw exception or return return null if pNode is null
+			if (IntPtr.Zero != pValue)
+			{
+				m_pValue = pValue;
+			}
+		}
+		public void Dispose()
+		{
+			Dispose(true);
+		}
+		
+		protected virtual void Dispose(bool bDisposing)
+		{
+			if (this.m_pValue != IntPtr.Zero)
+			{
+				this.m_pValue = IntPtr.Zero;
+			}
+			if (bDisposing)
+			{
+				// No need to call the finalizer since we've now cleaned
+				// up the unmanaged memory
+
+				GC.SuppressFinalize(this);
+			}
+		}
+		
+		~ZWaveValueList()
+		{
+			Dispose(false);
+		}
+		
+		#region Wrapper Methods
+				
+		public bool SetByLabel(string setVal)
+		{
+			return OPENZWAVEDLL_SetListByLabel(this.m_pValue, setVal);
+		}
+				
+		public bool SetByValue(Int32 setVal)
+		{
+			return OPENZWAVEDLL_SetListByValue(this.m_pValue, setVal);
+		}
+		
+		public void OnValueChanged(Int32 valueIdx)
+		{
+			OPENZWAVEDLL_OnValueChangedList(this.m_pValue, valueIdx);
+		}
+		
+		public string GetAsString()
+		{
+			StringBuilder strB = new StringBuilder(80);
+			OPENZWAVEDLL_GetAsStringList(this.m_pValue, strB);
+			return strB.ToString();			
+		}
+		
+		public ITEM GetItem()
+		{
+			return OPENZWAVEDLL_GetValueListItem(this.m_pValue);
+		}
+		
+		public ITEM GetPending()
+		{
+			return OPENZWAVEDLL_GetPendingListItem(this.m_pValue);
+		}
+		
+		public Int32 GetItemIdxByLabel(string strLabel)
+		{
+			return OPENZWAVEDLL_GetItemIdxByLabel(this.m_pValue, strLabel);
+		}
+				
+		public Int32 GetItemIdxByValue(Int32 val)
+		{
+			return OPENZWAVEDLL_GetItemIdxByValue(this.m_pValue, val);
+		}
+		
+		#endregion Wrapper Methods
+		
+	}
+}
