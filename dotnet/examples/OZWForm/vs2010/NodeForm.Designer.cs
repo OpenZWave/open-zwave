c28a4a37c6143beb4119fadee865e07659a3eea6
google@lansell.org <google@lansell.org>
2010-09-12 16:03:04 +0000

    Major re-arrangement of the repository, in order to allow multiple programming languages. Also combined src and h folders to make finding headers quicker. Added VS2010 projects for everything.
    Note: Various conflicts means this is going to take several commits.

---
+++ dotnet/examples/OZWForm/vs2010/NodeForm.Designer.cs
@@ -0,0 +1,70 @@
+﻿namespace OZWForm
+{
+    partial class NodeForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.NodeLayoutPanel = new System.Windows.Forms.TableLayoutPanel();
+            this.SuspendLayout();
+            // 
+            // NodeLayoutPanel
+            // 
+            this.NodeLayoutPanel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.NodeLayoutPanel.AutoScroll = true;
+            this.NodeLayoutPanel.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.NodeLayoutPanel.ColumnCount = 1;
+            this.NodeLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.NodeLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.NodeLayoutPanel.Location = new System.Drawing.Point(12, 12);
+            this.NodeLayoutPanel.Name = "NodeLayoutPanel";
+            this.NodeLayoutPanel.RowCount = 1;
+            this.NodeLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.NodeLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.NodeLayoutPanel.Size = new System.Drawing.Size(502, 305);
+            this.NodeLayoutPanel.TabIndex = 0;
+            // 
+            // NodeForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(526, 329);
+            this.Controls.Add(this.NodeLayoutPanel);
+            this.Name = "NodeForm";
+            this.ShowInTaskbar = false;
+            this.Text = "NodeForm";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel NodeLayoutPanel;
+    }
+}
