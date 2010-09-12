234b181014ca3422e8b424f518b6999152ae761d
google@lansell.org <google@lansell.org@db0adee0-878e-11de-bed6-6d316fbe0868>
2010-09-12 16:03:04 +0000

    Major re-arrangement of the repository, in order to allow multiple programming languages. Also combined src and h folders to make finding headers quicker. Added VS2010 projects for everything.
    Note: Various conflicts means this is going to take several commits.

---
+++ trunk/dotnet/examples/OZWForm/vs2010/NodeForm.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using OpenZWaveDotNet;
+
+namespace OZWForm
+{
+    public partial class NodeForm : Form
+    {
+        private Node m_node;
+
+        public NodeForm( Node node )
+        {
+            m_node = node;
+            InitializeComponent();
+
+            if( node.Manufacturer != "" )
+            {
+                this.Text = "Node " + node.ID.ToString() + ": " + node.Manufacturer + " " + node.Product;
+            }
+            else
+            {
+                this.Text = "Node " + node.ID.ToString() + ": " + node.Label;
+            }
+
+            foreach (ZWValueID valueID in node.Values)
+            {
+                Control control = null;
+                if (valueID.GetType() == ZWValueID.ValueType.Bool)
+                {
+                    control = new ValuePanelBool(valueID);
+                }
+                else if (valueID.GetType() == ZWValueID.ValueType.Byte)
+                {
+                    control = new ValuePanelByte(valueID);
+                }
+                if (control != null)
+                {
+                    NodeLayoutPanel.Controls.Add(control);
+                }
+            }
+        }
+    }
+}
