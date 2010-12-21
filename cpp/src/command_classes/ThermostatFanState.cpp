89aa8a5c8f9ff2b1c1c33e425aebb80cac2fe34d
google@lansell.org <google@lansell.org>
2010-12-21 19:19:40 +0000

    Removed attempt to obtain supported thermostat operating states and fan states.  These commands do not exist, and are the result of a false assumption.

--- cpp/src/command_classes/ThermostatFanState.cpp
+++ cpp/src/command_classes/ThermostatFanState.cpp
@@ -33,23 +33,34 @@
 #include "Driver.h"
 #include "Log.h"
 
-#include "ValueList.h"
+#include "ValueString.h"
 
 using namespace OpenZWave;
 
 enum ThermostatFanStateCmd
 {
 	ThermostatFanStateCmd_Get				= 0x02,
-	ThermostatFanStateCmd_Report			= 0x03,
-	ThermostatFanStateCmd_SupportedGet		= 0x04,
-	ThermostatFanStateCmd_SupportedReport	= 0x05
+	ThermostatFanStateCmd_Report			= 0x03
 };
 
 static char const* c_stateName[] = 
 {
 	"Idle",
-	"Running Low",
-	"Running High"
+	"Running",
+	"Running High",
+	"State 03",			// Undefined states.  May be used in the future.
+	"State 04",
+	"State 05",
+	"State 06",
+	"State 07",
+	"State 08",
+	"State 09",
+	"State 10",
+	"State 11",
+	"State 12",
+	"State 13",
+	"State 14",
+	"State 15"
 };
 
 
@@ -62,20 +73,6 @@ bool ThermostatFanState::RequestState
 	uint32 const _requestFlags
 )
 {
-	bool requests = false;
-	if( ( _requestFlags & RequestFlag_Static ) && HasStaticRequest( StaticRequest_Values ) )
-	{
-		// Request the supported states
-		Msg* msg = new Msg( "Request Supported Thermostat Fan States", GetNodeId(), REQUEST, FUNC_ID_ZW_SEND_DATA, true, true, FUNC_ID_APPLICATION_COMMAND_HANDLER, GetCommandClassId() );
-		msg->Append( GetNodeId() );
-		msg->Append( 2 );
-		msg->Append( GetCommandClassId() );
-		msg->Append( ThermostatFanStateCmd_SupportedGet );
-		msg->Append( TRANSMIT_OPTION_ACK | TRANSMIT_OPTION_AUTO_ROUTE );
-		GetDriver()->SendMsg( msg );
-		requests = true;
-	}
-
 	if( _requestFlags & RequestFlag_Dynamic )
 	{
 		// Request the current state
@@ -86,10 +83,10 @@ bool ThermostatFanState::RequestState
 		msg->Append( ThermostatFanStateCmd_Get );
 		msg->Append( TRANSMIT_OPTION_ACK | TRANSMIT_OPTION_AUTO_ROUTE );
 		GetDriver()->SendMsg( msg );
-		requests = true;
+		return true;
 	}
 
-	return requests;
+	return false;
 }
 
 //-----------------------------------------------------------------------------
@@ -106,42 +103,12 @@ bool ThermostatFanState::HandleMsg
 	if( ThermostatFanStateCmd_Report == (ThermostatFanStateCmd)_data[0] )
 	{
 		// We have received the thermostat fan state from the Z-Wave device
-		if( !m_supportedStates.empty() )
+		if( ValueString* valueString = m_state.GetInstance( _instance ) )
 		{
-			if( ValueList* valueList = m_state.GetInstance( _instance ) )
-			{
-				valueList->OnValueChanged( _data[1] );
-				Log::Write( "Received thermostat fan state from node %d: %s", GetNodeId(), valueList->GetItem().m_label.c_str() );		
-				valueList->Release();
-			}
+			valueString->OnValueChanged( c_stateName[_data[1]&0x0f] );
+			Log::Write( "Received thermostat fan state from node %d: %s", GetNodeId(), valueString->GetValue().c_str() );		
+			valueString->Release();
 		}
-		return true;
-	}
-	
-	if( ThermostatFanStateCmd_SupportedReport == (ThermostatFanStateCmd)_data[0] )
-	{
-		// We have received the supported thermostat fan states from the Z-Wave device
-		Log::Write( "Received supported thermostat fan states from node %d", GetNodeId() );		
-
-		m_supportedStates.clear();
-		for( uint32 i=1; i<_length-1; ++i )
-		{
-			for( int32 bit=0; bit<8; ++bit )
-			{
-				if( ( _data[i] & (1<<bit) ) != 0 )
-				{
-					ValueList::Item item;
-					item.m_value = (int32)((i-1)<<3) + bit;
-					item.m_label = c_stateName[item.m_value];
-					m_supportedStates.push_back( item );
-
-					Log::Write( "    Added fan state: %s", c_stateName[item.m_value] );
-				}
-			}
-		}
-
-		ClearStaticRequest( StaticRequest_Values );
-		CreateVars( _instance );
 		return true;
 	}
 		
@@ -157,14 +124,9 @@ void ThermostatFanState::CreateVars
 	uint8 const _instance
 )
 {
-	if( m_supportedStates.empty() )
-	{
-		return;
-	}
-
 	if( Node* node = GetNodeUnsafe() )
 	{
-		m_state.AddInstance( _instance, node->CreateValueList( ValueID::ValueGenre_User, GetCommandClassId(), _instance, 0, "State", "", true, m_supportedStates, m_supportedStates[0].m_value ) );
+		m_state.AddInstance( _instance, node->CreateValueString( ValueID::ValueGenre_User, GetCommandClassId(), _instance, 0, "Fan State", "", true, c_stateName[0] ) );
 	}
 }
 
