1ac99ca81a0228f7a0d512df75cea4b98bf44446
glsatz <glsatz@db0adee0-878e-11de-bed6-6d316fbe0868>
2012-02-04 07:48:04 +0000

    Mostly formatting and new types.

--- trunk/cpp/src/Defs.h
+++ trunk/cpp/src/Defs.h
@@ -94,24 +94,25 @@ namespace OpenZWave
 #define NAK											0x15
 #define CAN											0x18
 
-#define NUM_NODE_BITFIELD_BYTES						29		// 29 bytes = 232 bits, one for each possible node in the network.
+#define NUM_NODE_BITFIELD_BYTES							29		// 29 bytes = 232 bits, one for each possible node in the network.
 
-#define REQUEST										0x00
-#define RESPONSE									0x01
+#define REQUEST									0x00
+#define RESPONSE								0x01
 
 #define ZW_CLOCK_SET								0x30
 
 #define TRANSMIT_OPTION_ACK		 					0x01
-#define TRANSMIT_OPTION_LOW_POWER   				0x02
-#define TRANSMIT_OPTION_AUTO_ROUTE  				0x04
-#define TRANSMIT_OPTION_FORCE_ROUTE 				0x08
+#define TRANSMIT_OPTION_LOW_POWER		   				0x02
+#define TRANSMIT_OPTION_AUTO_ROUTE  						0x04
+#define TRANSMIT_OPTION_NO_ROUTE 						0x10
 
-#define TRANSMIT_COMPLETE_OK	  					0x00
-#define TRANSMIT_COMPLETE_NO_ACK  					0x01
-#define TRANSMIT_COMPLETE_FAIL						0x02
-#define TRANSMIT_COMPLETE_NOROUTE 					0x04
+#define TRANSMIT_COMPLETE_OK	  						0x00
+#define TRANSMIT_COMPLETE_NO_ACK	  					0x01
+#define TRANSMIT_COMPLETE_FAIL							0x02
+#define TRANSMIT_COMPLETE_NOT_IDLE						0x03
+#define TRANSMIT_COMPLETE_NOROUTE 						0x04
 
-#define RECEIVE_STATUS_TYPE_BROAD	 				0x04
+#define RECEIVE_STATUS_TYPE_BROAD	 					0x04
 
 #define FUNC_ID_SERIAL_API_GET_INIT_DATA		0x02
 #define FUNC_ID_APPLICATION_COMMAND_HANDLER		0x04
@@ -151,76 +152,92 @@ namespace OpenZWave
 #define FUNC_ID_ZW_IS_FAILED_NODE_ID			0x62	// Check to see if a specified node has failed
 #define FUNC_ID_ZW_REPLACE_FAILED_NODE			0x63	// Remove a failed node from the controller's list (?)
 #define FUNC_ID_ZW_GET_ROUTING_INFO			0x80	// Get a specified node's neighbor information from the controller
+#define FUNC_ID_SERIAL_API_SLAVE_NODE_INFO		0xA0	// Set application virtual slave node information
+#define FUNC_ID_APPLICATION_SLAVE_COMMAND_HANDLER	0xA1	// Slave command handler
+#define FUNC_ID_ZW_SEND_SLAVE_NODE_INFO			0xA2	// Send a slave node information frame
+#define FUNC_ID_ZW_SEND_SLAVE_DATA			0xA3	// Send data from slave
+#define FUNC_ID_ZW_SET_SLAVE_LEARN_MODE			0xA4	// Enter slave learn mode
+#define FUNC_ID_ZW_GET_VIRTUAL_NODES			0xA5	// Return all virtual nodes
+#define FUNC_ID_ZW_IS_VIRTUAL_NODE			0xA6	// Virtual node test
 #define FUNC_ID_ZW_SET_PROMISCUOUS_MODE			0xD0	// Set controller into promiscuous mode to listen to all frames
 #define FUNC_ID_PROMISCUOUS_APPLICATION_COMMAND_HANDLER		0xD1
 
-#define ADD_NODE_ANY								0x01
-#define ADD_NODE_CONTROLLER							0x02
-#define ADD_NODE_SLAVE								0x03
-#define ADD_NODE_EXISTING							0x04
-#define ADD_NODE_STOP								0x05
-#define ADD_NODE_STOP_FAILED						0x06
+#define ADD_NODE_ANY					0x01
+#define ADD_NODE_CONTROLLER				0x02
+#define ADD_NODE_SLAVE					0x03
+#define ADD_NODE_EXISTING				0x04
+#define ADD_NODE_STOP					0x05
+#define ADD_NODE_STOP_FAILED				0x06
 
-#define ADD_NODE_STATUS_LEARN_READY		  			0x01
-#define ADD_NODE_STATUS_NODE_FOUND		   			0x02
-#define ADD_NODE_STATUS_ADDING_SLAVE		 		0x03
-#define ADD_NODE_STATUS_ADDING_CONTROLLER			0x04
-#define ADD_NODE_STATUS_PROTOCOL_DONE				0x05
-#define ADD_NODE_STATUS_DONE				 		0x06
-#define ADD_NODE_STATUS_FAILED			   			0x07
+#define ADD_NODE_STATUS_LEARN_READY			0x01
+#define ADD_NODE_STATUS_NODE_FOUND			0x02
+#define ADD_NODE_STATUS_ADDING_SLAVE	 		0x03
+#define ADD_NODE_STATUS_ADDING_CONTROLLER		0x04
+#define ADD_NODE_STATUS_PROTOCOL_DONE			0x05
+#define ADD_NODE_STATUS_DONE				0x06
+#define ADD_NODE_STATUS_FAILED				0x07
 
-#define REMOVE_NODE_ANY								0x01
-#define REMOVE_NODE_STOP							0x05
+#define REMOVE_NODE_ANY					0x01
+#define REMOVE_NODE_STOP				0x05
 
-#define REMOVE_NODE_STATUS_LEARN_READY				0x01
-#define REMOVE_NODE_STATUS_NODE_FOUND				0x02
-#define REMOVE_NODE_STATUS_REMOVING_SLAVE			0x03
+#define REMOVE_NODE_STATUS_LEARN_READY			0x01
+#define REMOVE_NODE_STATUS_NODE_FOUND			0x02
+#define REMOVE_NODE_STATUS_REMOVING_SLAVE		0x03
 #define REMOVE_NODE_STATUS_REMOVING_CONTROLLER		0x04
-#define REMOVE_NODE_STATUS_DONE						0x06
-#define REMOVE_NODE_STATUS_FAILED					0x07
+#define REMOVE_NODE_STATUS_DONE				0x06
+#define REMOVE_NODE_STATUS_FAILED			0x07
 
-#define CREATE_PRIMARY_START						0x02
-#define CREATE_PRIMARY_STOP							0x05
-#define CREATE_PRIMARY_STOP_FAILED					0x06
+#define CREATE_PRIMARY_START				0x02
+#define CREATE_PRIMARY_STOP				0x05
+#define CREATE_PRIMARY_STOP_FAILED			0x06
 
-#define CONTROLLER_CHANGE_START						0x02
-#define CONTROLLER_CHANGE_STOP						0x05
-#define CONTROLLER_CHANGE_STOP_FAILED				0x06
+#define CONTROLLER_CHANGE_START				0x02
+#define CONTROLLER_CHANGE_STOP				0x05
+#define CONTROLLER_CHANGE_STOP_FAILED			0x06
 
-#define LEARN_MODE_STARTED							0x01
-#define LEARN_MODE_DONE								0x06	
-#define LEARN_MODE_FAILED							0x07
-#define LEARN_MODE_DELETED							0x80
+#define LEARN_MODE_STARTED				0x01
+#define LEARN_MODE_DONE					0x06	
+#define LEARN_MODE_FAILED				0x07
+#define LEARN_MODE_DELETED				0x80
 
-#define REQUEST_NEIGHBOR_UPDATE_STARTED				0x21
-#define REQUEST_NEIGHBOR_UPDATE_DONE				0x22
-#define REQUEST_NEIGHBOR_UPDATE_FAILED				0x23
+#define REQUEST_NEIGHBOR_UPDATE_STARTED			0x21
+#define REQUEST_NEIGHBOR_UPDATE_DONE			0x22
+#define REQUEST_NEIGHBOR_UPDATE_FAILED			0x23
 
-#define FAILED_NODE_OK								0x00
-#define FAILED_NODE_REMOVED							0x01
-#define FAILED_NODE_NOT_REMOVED						0x02
-#define FAILED_NODE_REPLACE_WAITING					0x03
-#define FAILED_NODE_REPLACE_DONE					0x04
-#define FAILED_NODE_REPLACE_FAILED					0x05
+#define FAILED_NODE_OK					0x00
+#define FAILED_NODE_REMOVED				0x01
+#define FAILED_NODE_NOT_REMOVED				0x02
+#define FAILED_NODE_REPLACE_WAITING			0x03
+#define FAILED_NODE_REPLACE_DONE			0x04
+#define FAILED_NODE_REPLACE_FAILED			0x05
 
-#define SUC_UPDATE_DONE								0x00
-#define SUC_UPDATE_ABORT							0x01
-#define SUC_UPDATE_WAIT								0x02
-#define SUC_UPDATE_DISABLED							0x03
-#define SUC_UPDATE_OVERFLOW							0x04
+#define SUC_UPDATE_DONE					0x00
+#define SUC_UPDATE_ABORT				0x01
+#define SUC_UPDATE_WAIT					0x02
+#define SUC_UPDATE_DISABLED				0x03
+#define SUC_UPDATE_OVERFLOW				0x04
 
-#define SUC_FUNC_BASIC_SUC							0x00
-#define SUC_FUNC_NODEID_SERVER						0x01
+#define SUC_FUNC_BASIC_SUC				0x00
+#define SUC_FUNC_NODEID_SERVER				0x01
 
-#define UPDATE_STATE_NODE_INFO_RECEIVED				0x84
-#define UPDATE_STATE_NODE_INFO_REQ_DONE				0x82
-#define UPDATE_STATE_NODE_INFO_REQ_FAILED			0x81
-#define UPDATE_STATE_ROUTING_PENDING				0x80
-#define UPDATE_STATE_NEW_ID_ASSIGNED				0x40
-#define UPDATE_STATE_DELETE_DONE					0x20
-#define UPDATE_STATE_SUC_ID							0x10
+#define UPDATE_STATE_NODE_INFO_RECEIVED			0x84
+#define UPDATE_STATE_NODE_INFO_REQ_DONE			0x82
+#define UPDATE_STATE_NODE_INFO_REQ_FAILED		0x81
+#define UPDATE_STATE_ROUTING_PENDING			0x80
+#define UPDATE_STATE_NEW_ID_ASSIGNED			0x40
+#define UPDATE_STATE_DELETE_DONE			0x20
+#define UPDATE_STATE_SUC_ID				0x10
 
-#define OPTION_HIGH_POWER							0x80
+#define SLAVE_ASSIGN_COMPLETE				0x00
+#define SLAVE_ASSIGN_NODEID_DONE			0x01	// Node ID has been assigned
+#define SLAVE_ASSIGN_RANGE_INFO_UPDATE			0x02	// Node is doing neighbor discovery
+
+#define SLAVE_LEARN_MODE_DISABLE			0x00	// disable add/remove virtual slave nodes
+#define SLAVE_LEARN_MODE_ENABLE				0x01	// enable ability to include/exclude virtual slave nodes
+#define SLAVE_LEARN_MODE_ADD				0x02	// add node directly but only if primary/inclusion controller
+#define SLAVE_LEARN_MODE_REMOVE				0x03	// remove node directly but only if primary/inclusion controller
+
+#define OPTION_HIGH_POWER				0x80
 
 //Device request related
 #define BASIC_SET									0x01
