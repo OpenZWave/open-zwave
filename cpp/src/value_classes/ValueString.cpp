733a2e803d5bd7596edcbd1ed423ea8a718d0856
glsatz <glsatz@gmail.com>
2012-12-24 08:00:10 +0000

    Address issue 118, IsSet value confusion. Do not pay attention to IsSet on write xml output. Value should have a default. If not we can address getting it a default. Dynamic values will never be correct. However configuration and write_only values will need to persist. Saving everything is easier and consistent.

--- cpp/src/value_classes/ValueString.cpp
+++ cpp/src/value_classes/ValueString.cpp
@@ -93,10 +93,7 @@ void ValueString::WriteXML
 )
 {
 	Value::WriteXML( _valueElement );
-	if ( !IsSet() )
-		_valueElement->SetAttribute( "value", "" );
-	else
-		_valueElement->SetAttribute( "value", m_value.c_str() );
+	_valueElement->SetAttribute( "value", m_value.c_str() );
 }
 
 //-----------------------------------------------------------------------------
