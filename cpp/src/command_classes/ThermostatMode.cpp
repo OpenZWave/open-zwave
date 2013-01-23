f38a1ad3bda1322a334497bef181c525f837f85c
glsatz <glsatz@gmail.com>
2013-01-23 07:00:44 +0000

    Fix ValueList using wrong value from various command classes. Prevent duplicate query stage complete notifications. Fix controller command bad memory references when removing node data or closing controller. Add a new notification for all nodes queried but found some dead nodes. Try to advertise that we support Scene Activation so handhelds will be willing to send it to us. Keep remove node from network from looping. Send remove node stop when removing node from network.

--- cpp/src/command_classes/ThermostatMode.cpp
+++ cpp/src/command_classes/ThermostatMode.cpp
@@ -306,7 +306,7 @@ bool ThermostatMode::SetValue
 	if( ValueID::ValueType_List == _value.GetID().GetType() )
 	{
 		ValueList const* value = static_cast<ValueList const*>(&_value);
-		uint8 state = (uint8)value->GetNewItem().m_value;
+		uint8 state = (uint8)value->GetItem().m_value;
 
 		Msg* msg = new Msg( "Set Thermostat Mode", GetNodeId(), REQUEST, FUNC_ID_ZW_SEND_DATA, true );
 		msg->Append( GetNodeId() );
