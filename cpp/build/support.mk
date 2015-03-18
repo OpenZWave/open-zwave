fe7f6d3f496ef4d7be2c44011ac951430472bd0e
Justin Hammond <justin@dynam.ac>
2015-03-18 13:48:08 +0800

    fix up Version number if git describe fails

--- cpp/build/support.mk
+++ cpp/build/support.mk
@@ -31,8 +31,13 @@ GIT		:= $(shell which git)
 ifeq ($(GIT),)
 VERSION_REV ?= 0
 else
-GITVERSION	:= $(shell $(GIT) describe --long --tags --dirty | sed s/^v//)
+GITVERSION	:= $(shell $(GIT) describe --long --tags --dirty 2>/dev/null | sed s/^v//)
+ifeq ($(GITVERSION),)
+GITVERSION	:= $(VERSION_MAJ).$(VERSION_MIN).-1
+VERSION_REV	:= 0
+else
 VERSION_REV 	?= $(shell echo $(GITVERSION) | awk '{split($$0,a,"-"); print a[2]}')
+endif
 endif
 ifeq ($(VERSION_REV),)
 VERSION_REV ?= 0
