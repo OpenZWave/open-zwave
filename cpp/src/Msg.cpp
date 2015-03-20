0341a63bc453ae9c859b4ba4b2178eb6dcc2bfb8
Justin Hammond <justin@dynam.ac>
2015-03-20 15:40:51 +0800

    Push Encryption Routines to their own file and out of the Classes (so they can be shared with different classes)

--- cpp/src/Msg.cpp
+++ cpp/src/Msg.cpp
@@ -30,6 +30,7 @@
 #include "Node.h"
 #include "Manager.h"
 #include "Utils.h"
+#include "ZWSecurity.h"
 #include "platform/Log.h"
 #include "command_classes/MultiInstance.h"
 #include "command_classes/Security.h"
@@ -65,7 +66,6 @@ Msg::Msg
 	m_length( 4 ),
 	m_targetNodeId( _targetNodeId ),
 	m_sendAttempts( 0 ),
-	m_sendNonceAttempts ( 0 ),
 	m_maxSendAttempts( MAX_TRIES ),
 	m_instance( 1 ),
 	m_endPoint( 0 ),
@@ -306,191 +306,13 @@ Driver* Msg::GetDriver
 uint8* Msg::GetBuffer() {
 	if (m_encrypted == false)
 		return m_buffer;
-#if 0
-	m_nonce[0] = 0x09;
-	m_nonce[1] = 0x0d;
-	m_nonce[2] = 0x93;
-	m_nonce[3] = 0xd3;
-	m_nonce[4] = 0x61;
-	m_nonce[5] = 0x61;
-	m_nonce[6] = 0x1d;
-	m_nonce[7] = 0xd6;
-#endif
-	uint8 len = 0;
-	e_buffer[len++] = SOF;
-	e_buffer[len++] = m_length + 18; // length of full packet
-	e_buffer[len++] = REQUEST;
-	e_buffer[len++] = FUNC_ID_ZW_SEND_DATA;
-	e_buffer[len++] = m_targetNodeId;
-	e_buffer[len++] = m_length + 11; 					// Length of the payload
-	e_buffer[len++] = Security::StaticGetCommandClassId();
-	e_buffer[len++] = SecurityCmd_MessageEncap;
-
-	/* create our IV */
-	uint8 initializationVector[16];
-	/* the first 8 bytes of a outgoing IV are random
-	 * and we add it also to the start of the payload
-	 */
-	for (int i = 0; i < 8; i++) {
-		//initializationVector[i] = (rand()%0xFF)+1;
-		initializationVector[i] = 0xAA;
-		e_buffer[len++] = initializationVector[i];
-	}
-	/* the remaining 8 bytes are the NONCE we got from the device */
-	for (int i = 0; i < 8; i++) {
-		initializationVector[8+i] = m_nonce[i];
-	}
-
-
-	uint8 plaintextmsg[32];
-	/* add the Sequence Flag
-	 * - Since we dont currently handle multipacket encryption
-	 * just set this to 0
-	 */
-	plaintextmsg[0] = 0;
-	/* now add the actual message to be encrypted */
-	for (int i = 0; i < m_length-6-3; i++)
-		plaintextmsg[i+1] = m_buffer[6+i];
-
-	/* now encrypt */
-	uint8 encryptedpayload[30];
-	aes_mode_reset(GetDriver()->GetEncKey());
-#ifdef DEBUG
-	PrintHex("Plain Text Packet:", plaintextmsg, m_length-5-3);
-#endif
-	if (aes_ofb_encrypt(plaintextmsg, encryptedpayload, m_length-5-3, initializationVector, GetDriver()->GetEncKey()) == EXIT_FAILURE) {
-		Log::Write(LogLevel_Warning, m_targetNodeId, "Failed to Encrypt Packet");
-		m_length = 0;
-		return NULL;
-	}
-#ifdef DEBUG
-	PrintHex("Encrypted Packet", encryptedpayload, m_length-5-3);
-#endif
-	/* now add the Encrypted output to the packet */
-	for (int i = 0; i < m_length-5-3; i++) {
-		e_buffer[len++] = encryptedpayload[i];
-	}
-
-	// Append the nonce identifier :)
-	e_buffer[len++] = m_nonce[0];
-
-
-	/* regenerate the IV */
-	for (int i = 0; i < 8; i++) {
-		//initializationVector[i] = (rand()%0xFF)+1;
-		initializationVector[i] = 0xAA;
-	}
-	/* the remaining 8 bytes are the NONCE we got from the device */
-	for (int i = 0; i < 8; i++) {
-		initializationVector[8+i] = m_nonce[i];
-	}
-
-	/* now calculate the MAC and append it */
-	uint8 mac[8];
-	GenerateAuthentication(&e_buffer[7], e_buffer[5], GetDriver()->GetControllerNodeId(), m_targetNodeId, initializationVector, mac);
-	for(int i=0; i<8; ++i )
-	{
-		e_buffer[len++] = mac[i];
-	}
-
-	e_buffer[len++] = GetDriver()->GetTransmitOptions();
-	/* this is the same as the Actual Message */
-	e_buffer[len++] = m_buffer[m_length-2];
-	// Calculate the checksum
-	uint8 csum = 0xff;
-	for( int32 i=1; i<len; ++i )
-	{
-		csum ^= e_buffer[i];
-	}
-	e_buffer[len++] = csum;
-	return e_buffer;
+	else
+		if (EncyrptBuffer(m_buffer, m_length, GetDriver(), GetDriver()->GetControllerNodeId(), m_targetNodeId, m_nonce, e_buffer)) {
+			return e_buffer;
+		} else {
+			Log::Write(LogLevel_Warning, m_targetNodeId, "Failed to Encyrpt Packet");
+			return NULL;
+		}
 }
 
-//-----------------------------------------------------------------------------
-// <Msg::GenerateAuthentication>
-// Generate authentication data from a security-encrypted message
-//-----------------------------------------------------------------------------
-bool Msg::GenerateAuthentication
-(
-	uint8 const* _data,				// Starting from the command class command
-	uint32 const _length,
-	uint8 const _sendingNode,
-	uint8 const _receivingNode,
-	uint8 *iv,
-	uint8* _authentication			// 8-byte buffer that will be filled with the authentication data
-)
-{
-	// Build a buffer containing a 4-byte header and the encrypted
-	// message data, padded with zeros to a 16-byte boundary.
-	uint8 buffer[256];
-	uint8 tmpauth[16];
-	memset(buffer, 0, 256);
-	memset(tmpauth, 0, 16);
-	buffer[0] = _data[0];							// Security command class command
-	buffer[1] = _sendingNode;
-	buffer[2] = _receivingNode;
-	buffer[3] = _length - 19; // Subtract 19 to account for the 9 security command class bytes that come before and after the encrypted data
-	memcpy( &buffer[4], &_data[9], _length-19 );	// Encrypted message
-
-	uint8 bufsize = _length - 19 + 4; /* the size of buffer */
-#ifdef DEBUG
-	PrintHex("Raw Auth (minus IV)", buffer, bufsize);
-	Log::Write(LogLevel_Debug, m_targetNodeId, "Raw Auth (Minus IV) Size: %d (%d)", bufsize, bufsize+16);
-#endif
-
-	aes_mode_reset(GetDriver()->GetAuthKey());
-	/* encrypt the IV with ecb */
-	if (aes_ecb_encrypt(iv, tmpauth, 16, GetDriver()->GetAuthKey()) == EXIT_FAILURE) {
-		Log::Write(LogLevel_Warning, m_targetNodeId, "Failed Initial ECB Encrypt of Auth Packet");
-		return false;
-	}
-
-	/* our temporary holding var */
-	uint8 encpck[16];
-
-	int block = 0;
-	/* reset our encpck temp var */
-	memset(encpck, 0, 16);
-	/* now xor the buffer with our encrypted IV */
-	for (int i = 0; i < bufsize; i++) {
-		encpck[block] = buffer[i];
-		block++;
-		/* if we hit a blocksize, then encrypt */
-		if (block == 16) {
-			for (int j = 0; j < 16; j++) {
-				/* here we do our xor */
-				tmpauth[j] = encpck[j] ^ tmpauth[j];
-				/* and reset encpck for good measure */
-				encpck[j] = 0;
-			}
-			/* reset our block counter back to 0 */
-			block = 0;
-			aes_mode_reset(GetDriver()->GetAuthKey());
-			if (aes_ecb_encrypt(tmpauth, tmpauth, 16, GetDriver()->GetAuthKey()) == EXIT_FAILURE) {
-				Log::Write(LogLevel_Warning, m_targetNodeId, "Failed Subsequent (%d) ECB Encrypt of Auth Packet", i);
-				return false;
-			}
-		}
-	}
-	/* any left over data that isn't a full block size*/
-	if (block > 0) {
-		for (int i= 0; i < 16; i++) {
-			/* encpck from block to 16 is already gauranteed to be 0
-			 * so its safe to xor it with out tmpmac */
-			tmpauth[i] = encpck[i] ^ tmpauth[i];
-		}
-		aes_mode_reset(GetDriver()->GetAuthKey());
-		if (aes_ecb_encrypt(tmpauth, tmpauth, 16, GetDriver()->GetAuthKey()) == EXIT_FAILURE) {
-			Log::Write(LogLevel_Warning, m_targetNodeId, "Failed Final ECB Encrypt of Auth Packet");
-			return false;
-		}
-	}
-	/* we only care about the first 8 bytes of tmpauth as the mac */
-#ifdef DEBUG
-	PrintHex("Computed Auth", tmpauth, 8);
-#endif
-	/* so only copy 8 bytes to the _authentication var */
-	memcpy(_authentication, tmpauth, 8);
-	return true;
-}
 
---
+++ cpp/src/ZWSecurity.h
@@ -0,0 +1,28 @@
+/*
+ * Security.h
+ *
+ *  Created on: Mar 20, 2015
+ *      Author: justinhammond
+ */
+
+#ifndef SECURITY_H_
+#define SECURITY_H_
+
+
+#include <cstdio>
+#include <string>
+#include <string.h>
+#include "Defs.h"
+#include "Driver.h"
+
+namespace OpenZWave
+{
+bool EncyrptBuffer( uint8 *m_buffer, uint8 m_length, Driver *driver, uint8 const _sendingNode, uint8 const _receivingNode, uint8 const m_nonce[8], uint8* e_buffer);
+
+bool GenerateAuthentication( uint8 const* _data, uint32 const _length, Driver *driver, uint8 const _sendingNode, uint8 const _receivingNode, uint8 *iv, uint8* _authentication);
+
+
+}
+
+
+#endif /* SECURITY_H_ */
