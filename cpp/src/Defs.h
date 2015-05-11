6da7374efc7d01050566debd270410afa5d32b6f
Justin Hammond <justin@dynam.ac>
2015-05-11 18:40:01 +0800

    replace remaining ControllerCommands with direct methods and Depreciate the ControllerCommand method in the Manager class

--- cpp/src/Defs.h
+++ cpp/src/Defs.h
@@ -54,6 +54,16 @@
 #	define OPENZWAVE_EXPORT_WARNINGS_ON
 #endif
 
+#ifdef __GNUC__
+#define DEPRECATED __attribute__((deprecated))
+#elif defined(_MSC_VER)
+#define DEPRECATED __declspec(deprecated)
+#else
+#pragma message("WARNING: You need to implement DEPRECATED for this compiler")
+#define DEPRECATED
+#endif
+
+
 #ifdef NULL
 #undef NULL
 #endif
