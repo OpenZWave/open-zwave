724da57c43fd8d9db97b9bf3d27c6b29ccd09166
J.A.P. Klessens <joost@ankhtechnologies.nl>
2015-05-15 13:46:40 +0200

    Modified the headers of the OZWForm project to the GNU General Public License

--- dotnet/examples/OZWForm/src/Node.cs
+++ dotnet/examples/OZWForm/src/Node.cs
@@ -8,20 +8,20 @@
 //
 //      SOFTWARE NOTICE AND LICENSE
 //
-//      This file is part of OpenZWave.
+//      This file is part of OZWForm.
 //
-//      OpenZWave is free software: you can redistribute it and/or modify
-//      it under the terms of the GNU Lesser General Public License as published
-//      by the Free Software Foundation, either version 3 of the License,
-//      or (at your option) any later version.
+//      OZWForm is free software: you can redistribute it and/or modify
+//      it under the terms of the GNU General Public License as published by
+//      the Free Software Foundation, either version 3 of the License, or
+//      (at your option) any later version.
 //
-//      OpenZWave is distributed in the hope that it will be useful,
+//      OZWForm is distributed in the hope that it will be useful,
 //      but WITHOUT ANY WARRANTY; without even the implied warranty of
 //      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//      GNU Lesser General Public License for more details.
+//      GNU General Public License for more details.
 //
-//      You should have received a copy of the GNU Lesser General Public License
-//      along with OpenZWave.  If not, see <http://www.gnu.org/licenses/>.
+//      You should have received a copy of the GNU General Public License
+//      along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //
 //-----------------------------------------------------------------------------
 
