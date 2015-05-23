784440feda0f6e3e757c9e6b821aefb7d77e4d86
xs4 <xs4@xs4me.net>
2015-05-23 19:08:53 +0200

    add initial wiring for SetDeviceClasses based on Zwave+ info report

--- cpp/src/Node.h
+++ cpp/src/Node.h
@@ -531,6 +531,7 @@ namespace OpenZWave
 
 
 		bool SetDeviceClasses( uint8 const _basic, uint8 const _generic, uint8 const _specific );	// Set the device class data for the node
+		bool SetDeviceClasses( uint8 const _role, uint8 const _nodeType );							// Set the device class data for the node based on the Zwave+ info report
 		bool AddMandatoryCommandClasses( uint8 const* _commandClasses );							// Add mandatory command classes as specified in the device_classes.xml to the node.
 		void ReadDeviceClasses();																	// Read the static device class data from the device_classes.xml file
 		string GetEndPointDeviceClassLabel( uint8 const _generic, uint8 const _specific );
@@ -538,6 +539,7 @@ namespace OpenZWave
 		static bool								s_deviceClassesLoaded;		// True if the xml file has alreayd been loaded
 		static map<uint8,string>				s_basicDeviceClasses;		// Map of basic device classes.
 		static map<uint8,GenericDeviceClass*>	s_genericDeviceClasses;		// Map of generic device classes.
+		static map<uint8,DeviceClass*>			s_roleDeviceClasses;		// Map of Zwave+ role device classes.
 
 	//-----------------------------------------------------------------------------
 	//	Statistics
