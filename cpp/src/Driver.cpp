ca123d66b5cc9cb85e05d3e175a8505f92abe79e
Justin Hammond <justin@dynam.ac>
2015-09-15 13:16:04 +0800

    Fixed issue #658 - Reset SendAttempts when moving messages to the Wakeup Queue, otherwise they will get dropped when the device wakes up as exceeding the retry count

--- cpp/src/Driver.cpp
+++ cpp/src/Driver.cpp
@@ -1310,6 +1310,9 @@ bool Driver::MoveMessagesToWakeUpQueue
 							if( !m_currentMsg->IsWakeUpNoMoreInformationCommand() && !m_currentMsg->IsNoOperation() )
 							{
 								Log::Write( LogLevel_Info, _targetNodeId, "Node not responding - moving message to Wake-Up queue: %s", m_currentMsg->GetAsString().c_str() );
+								/* reset the sendAttempts */
+								m_currentMsg->SetSendAttempts(0);
+
 								MsgQueueItem item;
 								item.m_command = MsgQueueCmd_SendMsg;
 								item.m_msg = m_currentMsg;
@@ -1347,6 +1350,8 @@ bool Driver::MoveMessagesToWakeUpQueue
 									if( !item.m_msg->IsWakeUpNoMoreInformationCommand() && !item.m_msg->IsNoOperation() )
 									{
 										Log::Write( LogLevel_Info, item.m_msg->GetTargetNodeId(), "Node not responding - moving message to Wake-Up queue: %s", item.m_msg->GetAsString().c_str() );
+										/* reset any SendAttempts */
+										item.m_msg->SetSendAttempts(0);
 										wakeUp->QueueMsg( item );
 									}
 									else
@@ -1361,6 +1366,9 @@ bool Driver::MoveMessagesToWakeUpQueue
 								if( _targetNodeId == item.m_nodeId )
 								{
 									Log::Write( LogLevel_Info, _targetNodeId, "Node not responding - moving QueryStageComplete command to Wake-Up queue" );
+									/* reset any SendAttempts */
+									item.m_msg->SetSendAttempts(0);
+
 									wakeUp->QueueMsg( item );
 									remove = true;
 								}
@@ -1370,6 +1378,9 @@ bool Driver::MoveMessagesToWakeUpQueue
 								if( _targetNodeId == item.m_cci->m_controllerCommandNode )
 								{
 									Log::Write( LogLevel_Info, _targetNodeId, "Node not responding - moving controller command to Wake-Up queue: %s", c_controllerCommandNames[item.m_cci->m_controllerCommand] );
+									/* reset any SendAttempts */
+									item.m_msg->SetSendAttempts(0);
+
 									wakeUp->QueueMsg( item );
 									remove = true;
 								}
