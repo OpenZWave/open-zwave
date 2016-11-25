7b7c10eb743a06e54076a4c00b7d573798b2b68e
Kame <tobias@codeatelier.com>
2016-11-25 08:26:29 +0100

    Fix ValueList::SetByValue for values not starting at 0 (#1035)

--- cpp/src/value_classes/ValueList.cpp
+++ cpp/src/value_classes/ValueList.cpp
@@ -225,7 +225,7 @@ bool ValueList::SetByValue
 {
 	// create a temporary copy of this value to be submitted to the Set() call and set its value to the function param
   	ValueList* tempValue = new ValueList( *this );
-	tempValue->m_valueIdx = _value;
+	tempValue->m_valueIdx = GetItemIdxByValue(_value);
 
 	// Set the value in the device.
 	bool ret = ((Value*)tempValue)->Set();
