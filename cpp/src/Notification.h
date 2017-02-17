8714b4bed8f8b18c6f94fb5fb0b6323b1f028792
Tobias <tobias@codeatelier.com>
2017-02-17 13:53:02 +0100

    Add getCommand to Notification Class
    This is usefull in the ControllerCommand notification to the command that triggered the Notification.
    see https://github.com/OpenZWave/open-zwave/issues/1108
    
    also added the ControllerCommand to the Notification::GetAsString()

--- cpp/src/Notification.h
+++ cpp/src/Notification.h
@@ -192,6 +192,12 @@ namespace OpenZWave
 		 */
 		uint8 GetNotification()const{ assert((Type_Notification==m_type) || (Type_ControllerCommand == m_type)); return m_byte; }
 
+        /**
+         * Get the (controller) command from a notification. Only valid for Notification::Type_ControllerCommand notifications.
+         * \return the (controller) command code.
+         */
+        uint8 GetCommand()const{ assert(Type_ControllerCommand == m_type); return m_command; }
+		
 		/**
 		 * Helper function to simplify wrapping the notification class.  Should not normally need to be called.
 		 * \return the internal byte value of the notification.
@@ -211,7 +217,7 @@ namespace OpenZWave
 		UserAlertNofification GetUserAlertType()const {return m_useralerttype;};
 
 	private:
-		Notification( NotificationType _type ): m_type( _type ), m_byte(0), m_event(0), m_useralerttype(Alert_None) {}
+		Notification( NotificationType _type ): m_type( _type ), m_byte(0), m_event(0), m_command(0), m_useralerttype(Alert_None) {}
 		~Notification(){}
 
 		void SetHomeAndNodeIds( uint32 const _homeId, uint8 const _nodeId ){ m_valueId = ValueID( _homeId, _nodeId ); }
@@ -223,11 +229,13 @@ namespace OpenZWave
 		void SetButtonId( uint8 const _buttonId ){ assert(Type_CreateButton==m_type||Type_DeleteButton==m_type||Type_ButtonOn==m_type||Type_ButtonOff==m_type); m_byte = _buttonId; }
 		void SetNotification( uint8 const _noteId ){ assert((Type_Notification==m_type) || (Type_ControllerCommand == m_type)); m_byte = _noteId; }
 		void SetUserAlertNofification(UserAlertNofification const alerttype){ assert(Type_UserAlerts==m_type); m_useralerttype = alerttype; }
+		void SetCommand( uint8 const _command ){ assert(Type_ControllerCommand == m_type); m_command = _command; }
 
 		NotificationType		m_type;
 		ValueID				m_valueId;
 		uint8				m_byte;
 		uint8				m_event;
+		uint8               m_command;
 		UserAlertNofification m_useralerttype;
 	};
 
