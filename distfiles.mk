50ba01d01186613f53a61a2e4929b88a5420f1d9
Nechry <nechry@gmail.com>
2017-11-07 16:12:38 +0100

    Add Dome Leak Sensor

--- distfiles.mk
+++ distfiles.mk
@@ -116,6 +116,7 @@ DISTFILES =	.gitignore \
 	config/dlink/dch-z510.xml \
 	config/dome/0002.xml \
 	config/dome/0083.xml \
+	config/dome/0085.xml \
 	config/dome/0087.xml \
 	config/dome/0088.xml \
 	config/dome/0101.xml \
