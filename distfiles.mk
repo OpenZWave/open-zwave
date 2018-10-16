cd05b73ad548193b1b646fcb9460dd782a91b6a5
Keith Pine <keith.pine@gmail.com>
2018-10-15 23:01:18 -0700

    Config file for HomeSeer FC200+ Fan Controller

--- distfiles.mk
+++ distfiles.mk
@@ -278,6 +278,7 @@ DISTFILES =	.gitignore \
 	config/heiman/HS1WL-Z.xml \
 	config/heiman/HS2SK-Z.xml \
 	config/homeseer/ezmotionplus.xml \
+	config/homeseer/hs-fc200plus.xml \
 	config/homeseer/hs-fls100plus.xml \
 	config/homeseer/hs-ls100plus.xml \
 	config/homeseer/hs-ms100plus.xml \
