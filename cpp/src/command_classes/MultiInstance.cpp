3fbcea77709f67369915d894aca65e3ff336cd95
Justin Hammond <justin@dynam.ac>
2018-11-14 13:25:49 +0800

    Related to issue #1638 - Source Endpoint 0 should be mapped to main CC

--- cpp/src/command_classes/MultiInstance.cpp
+++ cpp/src/command_classes/MultiInstance.cpp
@@ -688,6 +688,15 @@ void MultiInstance::HandleMultiChannelEncap
 		uint8 commandClassId = _data[3];
 		if( CommandClass* pCommandClass = node->GetCommandClass( commandClassId ) )
 		{
+			/* 4.85.13 - If the Root Device is originating a command to an End Point in another node, the Source End Point MUST be set to 0.
+			 *
+			 */
+			if (endPoint == 0) {
+				Log::Write( LogLevel_Error, GetNodeId(), "MultiChannelEncap with endpoint set to 0 - Send to Root Device");
+				pCommandClass->HandleMsg(&_data[4], _length-4);
+				return;
+			}
+
 			uint8 instance = pCommandClass->GetInstance( endPoint );
 			if( instance == 0 )
 			{
