f90e1cf657cebdd02e90151f3c542381ca890008
Justin Hammond <Justin@dynam.ac>
2019-05-17 19:59:01 +0800

    trim whitespace from the Notification names - Related to #1797

--- cpp/src/Utils.cpp
+++ cpp/src/Utils.cpp
@@ -64,7 +64,7 @@ string OpenZWave::ToLower
 // Remove WhiteSpaces from the begining and end of a string
 //-----------------------------------------------------------------------------
 
-std::string &OpenZWave::trim
+std::string &OpenZWave::removewhitespace
 (
 		std::string &s
 )
@@ -87,6 +87,21 @@ std::string &OpenZWave::trim
 	return s;
 }
 
+std::string& OpenZWave::ltrim(std::string& s) {
+  s.erase(s.begin(), std::find_if(s.begin(), s.end(),
+    std::ptr_fun<int, int>(std::isgraph)));
+  return s;
+}
+
+std::string& OpenZWave::rtrim(std::string& s) {
+  s.erase(std::find_if(s.rbegin(), s.rend(),
+    std::ptr_fun<int, int>(std::isgraph)).base(), s.end());
+  return s;
+}
+
+std::string& OpenZWave::trim(std::string& s) {
+  return OpenZWave::ltrim(OpenZWave::rtrim(s));
+}
 //-----------------------------------------------------------------------------
 // <OpenZWave::split>
 // Split a String into a vector, seperated by anything specified in seperators.
@@ -200,3 +215,5 @@ struct tm *localtime_r(time_t *_clock, struct tm *_result)
 	return _result;
 }
 #endif
+
+
