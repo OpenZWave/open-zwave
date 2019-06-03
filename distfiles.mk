768b7fdd80a4017985a01867854134d3725f84bf
Narek911 <narekmanukyan91@yahoo.com>
2019-06-03 06:38:36 +0000

    dist

--- distfiles.mk
+++ distfiles.mk
@@ -323,6 +323,9 @@ DISTFILES =	.gitignore \
 	config/heiman/HS1WL-Z.xml \
 	config/heiman/HS2SK-Z.xml \
 	config/heiman/HS2WD-Z.xml \
+	config/heltun/.he-zw-sw-5a-1.xml.swp \
+	config/heltun/he-zw-sw-5a-1.xml \
+	config/heltun/he-zw-therm-fc1.xml \
 	config/heltun/he-zw-therm-fl2.xml \
 	config/homeseer/ezmotionplus.xml \
 	config/homeseer/hs-fc200plus.xml \
