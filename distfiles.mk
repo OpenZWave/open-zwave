52bf6a89937d95955c4b71bbb1ec4a3032d82c08
Justin Hammond <Justin@dynam.ac>
2019-06-11 09:46:45 +0800

    Few Minor Fixes

--- distfiles.mk
+++ distfiles.mk
@@ -329,7 +329,6 @@ DISTFILES =	.gitignore \
 	config/heiman/HS1WL-Z.xml \
 	config/heiman/HS2SK-Z.xml \
 	config/heiman/HS2WD-Z.xml \
-	config/heltun/.he-zw-sw-5a-1.xml.swp \
 	config/heltun/he-zw-sw-5a-1.xml \
 	config/heltun/he-zw-therm-fc1.xml \
 	config/heltun/he-zw-therm-fl2.xml \
