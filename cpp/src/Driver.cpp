edc445e204f7bf90053e46f3bb71967b15e151bc
Peter Gebruers <peter.gebruers@gmail.com>
2019-11-19 17:05:15 +0100

    Add message details to log of dropped message
    Before PR
    
    Node002, ERROR: Dropping command, expected response not received after 1 attempt(s).
    
    After PR
    
    ode002, ERROR: Dropping command, expected response not received after 1 attempt(s). Command: "SensorMultilevelCmd_Get (Node=2): 0x01, 0x0b, 0x00, 0x13, 0x02, 0x04, 0x31, 0x04, 0x01, 0x00, 0x25, 0x20, 0xd0"

--- cpp/src/Driver.cpp
+++ cpp/src/Driver.cpp
@@ -1146,7 +1146,9 @@ bool Driver::WriteMsg(string const &msg)
 		else
 		{
 			// That's it - already tried to send GetMaxSendAttempt() times.
-			Log::Write(LogLevel_Error, nodeId, "ERROR: Dropping command, expected response not received after %d attempt(s)", m_currentMsg->GetMaxSendAttempts());
+			Log::Write(LogLevel_Error, nodeId, "ERROR: Dropping command, expected response not received after %d attempt(s). Command: \"%s\"",
+					   m_currentMsg->GetMaxSendAttempts(),
+					   m_currentMsg->GetAsString().c_str());
 		}
 		if (m_currentControllerCommand != NULL)
 		{
