be975ea15179b090ec77e7f8d75d4c6a545c0977
Nico Hartung <nicohartung1@googlemail.com>
2020-05-19 16:14:05 +0200

    Adds config for devolo Home Control Switch FM MT2759

--- distfiles.mk
+++ distfiles.mk
@@ -153,6 +153,7 @@ DISTFILES =	.gitignore \
 	config/devolo/mt2653.xml \
 	config/devolo/mt2756.xml \
 	config/devolo/mt2761.xml \
+	config/devolo/mt2759.xml \
 	config/devolo/rs014G0159.xml \
 	config/diehlcontrols/766366.xml \
 	config/dlink/dch-z110.xml \
@@ -497,6 +498,7 @@ DISTFILES =	.gitignore \
 	config/images/devolo/mt2652.png \
 	config/images/devolo/mt2756.png \
 	config/images/devolo/mt2761.png \
+	config/images/devolo/mt2759.png \
 	config/images/diehlcontrols/766366.png \
 	config/images/dlink/dch-z110.png \
 	config/images/dlink/dch-z120.png \
