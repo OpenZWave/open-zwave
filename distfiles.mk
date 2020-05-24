977ead6421c17822695bba4aad7b2730b7daa59f
Nico Hartung <nicohartung1@googlemail.com>
2020-05-24 17:20:57 +0200

    Adds config for devolo Home Control Dimmer FM MT2760

--- distfiles.mk
+++ distfiles.mk
@@ -154,6 +154,7 @@ DISTFILES =	.gitignore \
 	config/devolo/mt2756.xml \
 	config/devolo/mt2759.xml \
 	config/devolo/mt2761.xml \
+	config/devolo/mt2760.xml \
 	config/devolo/rs014G0159.xml \
 	config/diehlcontrols/766366.xml \
 	config/dlink/dch-z110.xml \
@@ -499,6 +500,7 @@ DISTFILES =	.gitignore \
 	config/images/devolo/mt2756.png \
 	config/images/devolo/mt2759.png \
 	config/images/devolo/mt2761.png \
+	config/images/devolo/mt2760.png \
 	config/images/diehlcontrols/766366.png \
 	config/images/dlink/dch-z110.png \
 	config/images/dlink/dch-z120.png \
